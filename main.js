5d851722bf3ab15da51227e4e5ccfcb61fae0ddd
shalabyelectronics <mhdshalabystar@gmail.com>
2024-02-12 20:56:01 +0300

    done from mutate method

--- main.js
+++ main.js
@@ -58,10 +58,24 @@ const pAequorFactory = (specimenNum , dna)=>{
       const randBaseIndex = Math.floor(Math.random() * this._dna.length) ;
       // Get the chosen Base
       let chosenBase = this._dna[randBaseIndex];
-      // Replace the dna base that we randomly selected with different base.
+      // Replace the first dna base that we randomly selected with different base.
 
-      console.log(chosenBase)
-      console.log()
+      // We will loop throw the dna array then check if we find the match of same base
+      for (i of this._dna){
+        if (i === chosenBase){
+          // If a match is found, we will initialize a new variable without assigning any value to it.
+          let newRandomBase;
+
+          // We will employ a do..while loop because our goal is to substitute the base we identified in the DNA with a different base.
+         do {
+          newRandomBase = returnRandBase()
+
+         }while(newRandomBase === i)
+          this._dna[this._dna.indexOf(i)] = newRandomBase
+          break
+        }
+      }
+      return this._dna
       // Replace the randBase with 
     }
   }
@@ -72,9 +86,11 @@ const pAequorFactory = (specimenNum , dna)=>{
 
 
 
+const dna = mockUpStrand()
+const test = pAequorFactory(1 , dna)
+const test2 = pAequorFactory(2, dna)
 
-const test = pAequorFactory(1 , mockUpStrand())
-const test2 = pAequorFactory(2, mockUpStrand())
+console.log(dna)
 console.log(test.mutate())
 // test2.specimenNum = '3'
 // Next check mutate that will change one base to another base that are different
